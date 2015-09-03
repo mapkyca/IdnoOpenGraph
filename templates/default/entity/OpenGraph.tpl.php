bada14a919b643257f354f8f9a5733fb33645002
Marcus Povey <marcus@marcus-povey.co.uk>
2015-09-03 15:01:42 +0100

    Fixes #2: Quick fix to handle instances where images are arrays.

--- templates/default/entity/OpenGraph.tpl.php
+++ templates/default/entity/OpenGraph.tpl.php
@@ -7,9 +7,16 @@
 		
 		<div class="idno-opengraph og">
 		
-				<?php if (!empty($ogp['og:image'])) { ?>
-					<img src="<?= $ogp['og:image']; ?>" class="og-image" />
-				<?php }?>
+				<?php if (!empty($ogp['og:image'])) { 
+				    
+				    $img = $ogp['og:image'];
+				    if (!is_array($img)) $img = [$img];
+				    
+				    ?>
+					<img src="<?= $img[0]; ?>" class="og-image" />
+				<?php 
+				    
+				}?>
 				<h3><a href="<?= $ogp['og:url']; ?>"><?= $ogp['og:title']; ?></a></h3>
 				<?= $ogp['og:description']; ?>
 		
